b61f4da86d0ef7aca41c1e2f4f1198732895ff4d
Triglav97 <vovay97@yandex.ru>
2021-10-18 18:42:52 +0300

    HW2_3_2

--- src/com/triglav/HW_2_3/Main.java
+++ src/com/triglav/HW_2_3/Main.java
@@ -1,12 +1,33 @@
 package com.triglav.HW_2_3;
 
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
-    public static void main(String[] args) {
-        Random rand = new Random();
-        List<String> words = new ArrayList<>(randMassWords(rand.nextInt(20-10)+10));
-        repeatsWord(words);
+    public static void main(String[] args){
+//        Random rand = new Random();
+//        List<String> words = new ArrayList<>(randMassWords(rand.nextInt(20-10)+10));
+//        repeatsWord(words);
+        PhoneBook pb = new PhoneBook();
+        pb.add("+7(926)768-14-57", "Ярославский");
+        pb.add("+7(926)768-14-58", "Кузьмин");
+        pb.add("+7(926)768-14-59", "Викулин");
+        pb.add("+7(926)768-14-60", "Павлов");
+        pb.add("+7(926)768-14-6", "Егоров");
+        pb.add("+7(926)768-14-62", "Степанов");
+        pb.add("+7(926)768-14-63", "Павлов");
+        pb.add("+7(926)768-14-64", "Ярославский");
+        pb.add("+7(926)768-14-65", "Егоров");
+        pb.add("+7(926)768-14-66", "Степанов");
+        pb.add("+7(926)768-14-67", "Ярославский");
+
+        pb.get("Ярославский");
+        pb.get("Зиновьев");
+        pb.get("Егоров");
+        pb.get("Степанов");
+        pb.get("Павлов");
+
     }
 
     public static void repeatsWord(List<String> words) {
