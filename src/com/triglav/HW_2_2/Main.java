89f46b4b610a7d5476fd9aa59a3a13c333062ba9
Triglav97 <vovay97@yandex.ru>
2021-10-18 12:27:44 +0300

    HW2_2_2 - HW2_2_3

--- src/com/triglav/HW_2_2/Main.java
+++ src/com/triglav/HW_2_2/Main.java
@@ -1,6 +1,8 @@
 package com.triglav.HW_2_2;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -17,32 +19,41 @@ public class Main {
                                 {"1","2","3","4"},
                                 {"1","2","imposter","4"},
                                 {"1","2","3","4"}};
-        for (String[] i: arrTrue) {
-            for (String k : i){
-                System.out.print(k + " ");
+
+        List<String[][]> arrays = new ArrayList<>();
+        arrays.add(arrTrue);
+        arrays.add(arrFalse);
+        arrays.add(arr2False);
+
+        for(String[][] arr : arrays){
+            viewArr(arr);
+            try {
+                try{
+                    int res = method(arr);
+                    System.out.println("Сумма значений массива равна: " + res);
+                }catch (MyArraySizeException e) {
+                    System.out.println(e.getMessage());
+                }
+            } catch (MyArrayDataException e) {
+                System.out.println(e.getMessage());
+                System.out.println("в ячейке: [" + e.getI() + "][" + e.getJ() + "]");
             }
             System.out.println();
         }
-        System.out.println(arrTrue.length);
-
-//        try {
-//            int i = 0;
-//            if (i==0) {
-//                throw new MyArraySizeException("параметр");
-//            }
-//        }catch (MyArraySizeException e){
-//            System.out.println(e.getMessage() + " прошло");
-//        }
-
-        try{
-            method(arr2False);
-        }catch (MyArraySizeException e) {
-            e.printStackTrace();
-        }
-
     }
 
-    public static void method(String[][] arr) throws MyArraySizeException{
+    public static void viewArr(String [][] arr){
+        for (String[] i : arr) {
+            for (String k : i) {
+                System.out.print(k + " ");
+            }
+            System.out.println(" размер строки: " + i.length);
+        }
+        System.out.println("кол-во строк: " + arr.length);
+    }
+
+    public static int method(String[][] arr) throws MyArraySizeException, MyArrayDataException{
+        int res = 0;
         if (arr.length != 4) {
             throw new MyArraySizeException("Не верный размер массива");
         }
@@ -50,6 +61,14 @@ public class Main {
             if (arr[i].length != 4) {
                 throw new MyArraySizeException("Не верный размер массива");
             }
+            for (int j=0;j< arr.length; j++){
+                try{
+                    res = res + Integer.parseInt(arr[i][j]);
+                } catch (NumberFormatException e) {
+                    throw new MyArrayDataException(i, j);
+                }
+            }
         }
+        return res;
     }
 }
